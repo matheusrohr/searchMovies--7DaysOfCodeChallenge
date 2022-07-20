7d3d6db3ed3948e8f5ba1d0000ba08fba43842cb
matheusrohr <matheus.rohr@gmail.com>
2022-07-20 19:48:34 -0300

    criada função de busca usando o click no icone ou  tecla de enter. foram itilizados os metodos fetch e addEventListener

--- js/app.js
+++ js/app.js
@@ -1,63 +1,60 @@
 import { keyApi } from "./key.js";
-import { movies } from "./movies.js";
 
-async function fetchData(){
+
+async function getPopularMovies() {
   const responce = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${keyApi}&language=en-US&page=1`)
   const data = await responce.json();
   const setMovies = data.results;
 
-  const title = setMovies?.title
-  const rating = setMovies?.vote_average
-  const isFavorited = setMovies?.favorito
-  console.log(`${title} - ${rating} - ${isFavorited}`)
   return setMovies
 }
 
-async function searchData(){
-  const responce = await fetch(`https://api.themoviedb.org/3/search/company${keyApi}&query=marvel`)
+async function searchMovies() {
+  const titleSearchMovies = document.querySelector("#buscamovie").value
+  console.log(titleSearchMovies + "Estêvão")
+  const responce = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${keyApi}&language=en-US&page=1&include_adult=false&query=${titleSearchMovies}`)
   const data = await responce.json();
+  const searchMoviesdata = data.results;
 
-  console.log('Dados ==3> ', data);
+  console.log(`Olá de novo ${titleSearchMovies}`)
+
+  return searchMoviesdata
 }
 
-//const moviesApi = fetch("https://api.themoviedb.org/3/search/company/550?api_key=f66f510e67f6d4948262fed0ba5acada&query=marvel").then(response => response.json).then(data => {
-//  title = data.title  
-//})  
-//console.log(moviesApi.title)
-
 let favorito = (isFavorited) => {
-  if (isFavorited){
+  if (isFavorited) {
     return '/img/icon/true-heart.svg'
-  }else {
+  } else {
     return '/img/icon/false-heart.svg'
   }
 }
 
-let criarCard = (movies , i) => {
+let CreateMoviePoster = (movies) => {
+
   let box = document.querySelector('div#box')
-  
+
   let card = document.createElement('div')
   card.className = 'card'
-  
-  let iconTitle = document.createElement('div')//.setAttribute('class', 'icon-title')
+
+  let iconTitle = document.createElement('div')
   iconTitle.className = 'icon-title'
-  
+
   let divImg = document.createElement('div')
   let imgCapa = document.createElement('img')
   imgCapa.className = 'capa-movie'
   imgCapa.src = `https://image.tmdb.org/t/p/w500${movies.poster_path}`
   imgCapa.alt = movies.title
-  
+
   let avali = document.createElement('div')
   avali.className = 'avali'
-  
+
   let divTitulo = document.createElement('div')
   let pTitulo = document.createElement('p')
   let pTexto = document.createTextNode(`${movies.title} (${movies.release_date})`)
-  
+
   let icon = document.createElement('div')
   icon.className = 'icon'
-  
+
   let divIconeStar = document.createElement('div')
   let imgIconStar = document.createElement('img')
   imgIconStar.src = '/img/icon/star.svg'
@@ -74,7 +71,7 @@ let criarCard = (movies , i) => {
   descricao.className = 'descricao'
   let spanDescricao = document.createElement('span')
   let textoSpan = document.createTextNode(movies.overview)
-  
+
   divImg.appendChild(imgCapa)
   iconTitle.appendChild(divImg)
   pTitulo.appendChild(pTexto)
@@ -99,11 +96,33 @@ let criarCard = (movies , i) => {
 
 }
 
-//movies.forEach(movies => criarCard(movies))
+getPopularMovies().then(
+  function (setMovie) { setMovie.forEach(setMovie => CreateMoviePoster(setMovie)) },
 
-fetchData().then(
-    function(setMovie) { setMovie.forEach(setMovie => criarCard(setMovie))},
+);
 
+const searchMovieButton = document.querySelector("#search-icon")
+const searchMovieInput = document.querySelector("#buscamovie")
+
+searchMovieButton.addEventListener('click', function () {
+  const cardBox = document.querySelector("#box");
+  cardBox.innerHTML = ""
+
+  searchMovies().then(
+    function (searchMoviesdata) { searchMoviesdata.forEach(searchMoviesdata => CreateMoviePoster(searchMoviesdata)) },
+  );
+}
 )
 
-console.log(fetchData())
+searchMovieInput.addEventListener("keydown", function (e) {
+
+  if (e.keyCode == 13) {
+    const cardBox = document.querySelector("#box");
+    cardBox.innerHTML = ""
+
+    searchMovies().then(
+      function (searchMoviesdata) { searchMoviesdata.forEach(searchMoviesdata => CreateMoviePoster(searchMoviesdata)) },
+    );
+  }
+}
+)
